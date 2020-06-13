681209e91ef4791bf2a5f81ecc72912890b1ca3c
Le Thanh Tung <42868337+LeThanhTung17@users.noreply.github.com>
2020-06-13 10:43:02 +0700

    let cat eat mouse, dog eat cat =))

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var Dog = require('./Dog');
+var Cat = require('./Cat');
+var Mouse = require('./Mouse');
 
 var dog = new Dog("Tom");
+var cat = new Cat('Meo');
+var mouse = new Mouse('Mickey');
 
+cat.eat(mouse);
 dog.sayHi()
+dog.eat(cat);
+console.log(cat)
+console.log(dog)
