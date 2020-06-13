22543f148ec8435bdeb2fe9adc0b1c7ff8841bbe
Le Thanh Tung <42868337+LeThanhTung17@users.noreply.github.com>
2020-06-13 10:08:58 +0700

    change name method die

--- Mouse.js
+++ Mouse.js
@@ -1,6 +1,11 @@
 function Mouse(name, age) {
 	this.name = name;
-	this.age = age
+	this.age = age;
+	this.isDead = false;
 }
+
+Mouse.prototype.die = function() {
+	this.isDead = true;
+};
 
 module.exports = Mouse;
